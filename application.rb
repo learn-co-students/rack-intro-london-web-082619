9a009bc9f78ee877984f56e7b6d428498fdf5876
Abdullah Bajaman <ambajaman@gmail.com>
2019-09-13 23:58:31 +0100

    Done.

--- application.rb
+++ application.rb
@@ -1,8 +1,9 @@
+require 'pry'
 class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Abdullah"
     resp.finish
   end
 
